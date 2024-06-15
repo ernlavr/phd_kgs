7706caeb8fc6bd54ac3ef528dc5d9c53148521bd
ErnestsLavrinovicsalias] <ernests.lavrinovics@gmail.com>
2024-06-15 16:27:20 +0200

    fix cached bug

--- src/processing/owl_vec_alignment.py
+++ src/processing/owl_vec_alignment.py
@@ -1,5 +1,6 @@
 from owl2vec_star import owl2vec_star as o2v
 from gensim.models import KeyedVectors
+import os
 
 
 def _try_get_cached_model(path):
@@ -9,12 +10,12 @@ def _try_get_cached_model(path):
         return None
     
 def get_embeddings(kg, recompute=False):
-    cached = _try_get_cached_model(f"{kg.getName}_ontology.embeddings")
+    cached = _try_get_cached_model(os.path.join("output", f"{kg.getName()}_ontology.embeddings"))
     if cached is not None and recompute is False:
         return cached
 
     model = o2v.extract_owl2vec_model(kg.path, "res/default.cfg", True, True, True)
-    model.save(f"{kg.getName}_ontology.embeddings")
+    model.save(os.path.join("output", f"{kg.getName()}_ontology.embeddings"))
     return model
 
 
