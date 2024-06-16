fdd0a6a0b78e86748514447da53776a82ad1a16b
ErnestsLavrinovicsalias] <ernests.lavrinovics@gmail.com>
2024-06-16 15:30:54 +0200

    Add docs

--- src/processing/owl_vec_alignment.py
+++ src/processing/owl_vec_alignment.py
@@ -8,6 +8,7 @@ import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from tqdm import tqdm
+import gensim
 
 
 
@@ -27,7 +28,16 @@ def get_embeddings(kg, config, recompute=False):
     model.save(os.path.join("output", f"{kg.getName()}_ontology.embeddings"))
     return model
 
-def get_properties_classes(model):
+def get_properties_classes(model: gensim.models.word2vec.Word2Vec):
+    """
+    Parameters:
+        model: The Word2Vec model from which to extract properties and classes.
+
+    Returns:
+        properties (dict): Keys are property URIs and values are their corresponding vectors.
+        classes (dict): Keys are class URIs and values are their corresponding vectors.
+    """
+
     wv = model.wv
     properties = {}
     classes = {}
@@ -54,44 +64,36 @@ def _compute_cosines(kg1, kg2, threshold):
     return equals
 
 def get_class_alignment(kg_1, kg_2, output):
-    print("Implement owl2vec")
+    # Retrieve embeddings
     model_kg_1 = get_embeddings(kg_1, "res/default_kg1.cfg")
     model_kg_2 = get_embeddings(kg_2, "res/default_kg2.cfg")
 
+    # Retrieve properties and classes
     props_kg1, classes_kg1 = get_properties_classes(model_kg_1)
     props_kg2, classes_kg2 = get_properties_classes(model_kg_2)
 
-
+    # Get equals according to cosine similarity
     equal_classes = _compute_cosines(classes_kg1, classes_kg2, 0.8)
     equal_props = _compute_cosines(props_kg1, props_kg2, 0.8)
 
+    # Serialize
     serialize_into_graph(equal_classes, equal_props, output)
-    
-    """df_kg1 = pd.DataFrame.from_dict(embedding_dict_kg_1, orient='index')
-    df_kg2 = pd.DataFrame.from_dict(embedding_dict_kg_2, orient='index')
-
-    kg1PC = get_pca(df_kg1, 30)
-    kg2PC = get_pca(df_kg2, 30)
 
 
-    pairwise_matrix = cosine_similarity(df_kg1, df_kg2)
-    pairwise_matrix_cossim_PC = cosine_similarity(kg1PC, kg2PC)
+def serialize_into_graph(equal_classes: list, equal_properties: list, output_name: str):
+    """
+    This function takes in two lists of equivalent classes and properties, and an output name. 
+    It creates a graph using these classes and properties, and then serializes this graph into a 
+    Turtle (ttl) format file with the given output name.
 
+    Parameters:
+    equal_classes (list): A list of tuples where each tuple contains two equivalent classes.
+    equal_properties (list): A list of tuples where each tuple contains two equivalent properties.
+    output_name (str): The name of the output file where the serialized graph will be saved.
 
-    for i, kg2_row in enumerate(pairwise_matrix_cossim_PC):
-        # get kg2_row all indices that are above 0.9
-        indices = np.where(kg2_row > 0.8)
-        if len(indices[0]) == 0:
-            continue
-
-        max_idx = max(indices[0])
-        kg1_class = df_kg1.iloc[i].name
-        kg2_class = df_kg2.iloc[max_idx].name
-        print(f"{kg1_class} -> {kg2_class} - {kg2_row[max_idx]}")"""
-
-    pass
-
-def serialize_into_graph(equal_classes, equal_properties, output_name):
+    Returns:
+    g (rdflib.Graph): The created and serialized graph.
+    """
     g = Graph()
     # Pair-wise compare classes
     for i in tqdm(equal_classes, "Classes.."):
@@ -108,15 +110,4 @@ def serialize_into_graph(equal_classes, equal_properties, output_name):
     os.makedirs(os.path.dirname(output_name), exist_ok=True)
     g.serialize(destination=output_name, format='ttl')
     return g
-
-
-def get_pca(df, dim):
-    scaler = StandardScaler()
-    scaled_df = scaler.fit_transform(df)
-    #print (scaled_df[1].mean(), scaled_df[1].std())
-    pca = PCA(n_components=dim)
-    principalComponents = pca.fit_transform(scaled_df)
-    principalDf = pd.DataFrame(data = principalComponents)
-    print(pca.explained_variance_ratio_.sum(), "Cumulative Variance Ratio")
-    return principalDf
 
